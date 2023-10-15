154bef2689b3d127e945d2630376f16cc9013646
Nicolás Carvajal <n.carvajalc@uniandes.edu.co>
2023-10-14 23:50:23 -0500

    Uploaded initial project template

---
+++ tasks/convertions.py
@@ -0,0 +1,8 @@
+from celery import Celery
+from settings import Settings
+
+settings = Settings()
+REDIS_HOST = settings.REDIS_HOST
+REDIS_PORT = settings.REDIS_PORT
+
+celery_app = Celery(__name__, broker=f"redis://{REDIS_HOST}:{REDIS_PORT}/0")
