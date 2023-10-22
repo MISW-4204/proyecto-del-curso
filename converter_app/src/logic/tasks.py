bc9b4dbddce84e96449ad78e57fe2523ccd9a138
veronica <v.escobar1@uniandes.edu.co>
2023-10-22 16:20:22 -0500

    Basic file conversion methods and also database querys

--- converter_app/src/logic/tasks.py
+++ converter_app/src/logic/tasks.py
@@ -1,4 +1,8 @@
 from moviepy.editor import VideoFileClip
+from sqlalchemy.orm import Session
+
+from src.models.task import Task
+from src.schemas.task import ConversionTaskBase
 
 
 def convert_video(input_file: str, output_format: str) -> str:
@@ -14,3 +18,16 @@ def convert_video(input_file: str, output_format: str) -> str:
     clip.write_videofile(output_file, codec='libx264' if output_format == 'mp4' else None)
 
     return output_file
+
+
+def get_all_tasks(db: Session):
+    # Query the database to get all ongoing conversion tasks
+    tasks = db.query(Task).all()
+    #TODO: CONVERT THIS TO THE SCHEMA FORMAT
+    return tasks
+
+
+def get_task_by_id(db:Session, id_task):
+    # Retrieve the specific conversion task from the database
+    task = db.query(Task).filter(Task.id == id_task).first()
+    return ConversionTaskBase(task)
